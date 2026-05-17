7ed14244d88c3a445a48f304cb3e29fe67426cbf
v.sementsov <v.sementsov@andersenlab.com>
2026-05-17 16:39:44 +0200

    fix images

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "ac.goit.global" }],
+  },
   /* config options here */
   reactCompiler: true,
 };
